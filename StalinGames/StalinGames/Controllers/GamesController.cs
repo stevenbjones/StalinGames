acbb3aab9fc0413e3c0a833e94bf0ffe6c2c1419
Jensyboy <jensvandesteen123@gmail.com>
2020-05-17 19:44:18 +0200

    lol

--- StalinGames/StalinGames/Controllers/GamesController.cs
+++ StalinGames/StalinGames/Controllers/GamesController.cs
@@ -2,29 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StalinGames.DAL.Models;
+using StalinGames.DAL.Repositories;
 using StalinGames.ViewModels;
 
 namespace StalinGames.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IPlayerPurchasesRepository _playerPurchasesRepository;
+        private readonly IPlayerItemRepository _playerItemRepository;
+
+        public GamesController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager, IWebHostEnvironment webHostEnvironment,
+            IPlayerPurchasesRepository playerPurchasesRepository, IPlayerItemRepository playerItemRepository)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _webHostEnvironment = webHostEnvironment;
+            _playerPurchasesRepository = playerPurchasesRepository;
+            _playerItemRepository = playerItemRepository;
+        }
+
         [HttpGet]
         public async Task<IActionResult> SlotMachine()
         {
-            Random random = new Random();
-            int getal = random.Next(0, 100);
-            SlotMachineViewModel model = new SlotMachineViewModel()
-            {
-                random = getal
-            };
-            return View(model);
+
+            ApplicationUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+            return View(user);
         }
 
         [HttpPost]
-        public IActionResult SlotMachine(int getal)
+        public async Task<IActionResult> SlotMachine(int getal)
         {
-            getal = getal * 2;
+            ApplicationUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+            user.Blyats += getal;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return Json(user.Blyats);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Error while updating user blyats";
+                return View("Error");
+            }
+                
+            
+            //return Ok("Success");
+        }
+
+        [HttpPost]
+        public IActionResult SlotMachineTest(int getal)
+        {
+            getal = 69420;
             return Json(getal);
             //return Ok("Success");
         }
